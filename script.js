7be8995dd5faa4e6dc733afe30e6bdd02dbbfbb7
VeronaPl <vp31102003@gmail.com>
2023-12-11 01:29:18 +0300

    add try catch of errors

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const input = document.querySelector('.input-file input[type=file]');
+// const main = document.querySelector('.main');
 input.addEventListener('change', function(event) {
 	let file = event.target.files[0];
     let reader = new FileReader();
     reader.readAsText(file, 'UTF-8');
-    alert(file.name);
+    // alert(file.name);
+    // main.insertAdjacentHTML('beforebegin', '<div class="header"><span>Неправильный формат файла, разрешены только файлы .CSV</span></div>');
     reader.onload = () => {
         console.log(reader.result);
     };
     reader.onerror = () => {
-        console.log(reader.error);
+        try{
+            console.log(reader.error);
+            document.getElementsByClassName('.header').style.visibility = "visible";
+        }catch (err){
+            console.log(err);
+        }
+        
     };
 	// document.querySelector('.input-file span').next().html(file.name);
 });
